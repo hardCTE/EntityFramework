72b35b5d21f7e0dcaba5c2c5e3764aa6e47d0587
Micah Kary <mikary@microsoft.com>
2015-03-23 15:53:23 -0700

    :lock: Sanatize SQL generated by SqlServerSequenceValueGenerator

--- src/EntityFramework.SqlServer/SqlServerSequenceValueGenerator.cs
+++ src/EntityFramework.SqlServer/SqlServerSequenceValueGenerator.cs
@@ -13,28 +13,30 @@ namespace Microsoft.Data.Entity.SqlServer
     public class SqlServerSequenceValueGenerator<TValue> : HiLoValueGenerator<TValue>
     {
         private readonly SqlStatementExecutor _executor;
+        private readonly ISqlServerSqlGenerator _sqlGenerator;
         private readonly ISqlServerConnection _connection;
         private readonly string _sequenceName;
 
         public SqlServerSequenceValueGenerator(
             [NotNull] SqlStatementExecutor executor,
+            [NotNull] ISqlServerSqlGenerator generator,
             [NotNull] SqlServerSequenceValueGeneratorState generatorState,
             [NotNull] ISqlServerConnection connection)
-            : base(generatorState)
+            : base(Check.NotNull(generatorState, nameof(generatorState)))
         {
             Check.NotNull(executor, nameof(executor));
-            Check.NotNull(generatorState, nameof(generatorState));
+            Check.NotNull(generator, nameof(generator));
             Check.NotNull(connection, nameof(connection));
 
             _sequenceName = generatorState.SequenceName;
             _executor = executor;
+            _sqlGenerator = generator;
             _connection = connection;
         }
 
         protected override long GetNewLowValue()
         {
-            // TODO: Parameterize query and/or delimit identifier without using SqlServerMigrationOperationSqlGenerator
-            var sql = string.Format(CultureInfo.InvariantCulture, "SELECT NEXT VALUE FOR {0}", _sequenceName);
+            var sql = "SELECT NEXT VALUE FOR " + _sqlGenerator.DelimitIdentifier(_sequenceName);
             var nextValue = _executor.ExecuteScalar(_connection, _connection.DbTransaction, sql);
 
             return (long)Convert.ChangeType(nextValue, typeof(long), CultureInfo.InvariantCulture);
